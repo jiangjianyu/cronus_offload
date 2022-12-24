07168be214877b0877d49ab4b96ef1abcbea87d2
jianyu-m <jianyu@hku.hk>
2022-12-24 22:12:27 +0800

    refactor structure

--- src/cudart/cuda_runtime_header.h
+++ src/cudart/cuda_runtime_header.h
@@ -15,6 +15,8 @@ extern FatBinary *fatbin_handle;
 #define cudart_log_err(format, ...) log_err("CUDART: " format, ## __VA_ARGS__)
 #define cudart_log_call() cudart_log_info("=> %s", __FUNCTION__)
 #define cudart_exit() exit(1)
+#define cudart_not_implemented(ret) cudart_log_warn("%s not implemented", __FUNCTION__); return ret
+#define cudart_not_implemented_noreturn cudart_log_warn("%s not implemented", __FUNCTION__)
 
 #define NOT_IMPLEMENTED cudart_log_warn("%s not implemented", __FUNCTION__)
 #define NOT_IMPLEMENTED_RET(x) cudart_log_warn("%s not implemented", __FUNCTION__); return x;
